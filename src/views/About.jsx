7f38876c6789303e9c1fbc451e9fca737c2dffaa
colombelli <xcolombelli@gmail.com>
2021-07-06 20:46:31 -0300

    Update about page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {
     Card,
+    CardImg,
+    CardBody,
     CardHeader,
     CardTitle,
     Row,
@@ -16,11 +18,43 @@ function About() {
             <Col>
             <Card>
             <CardHeader>
-                <CardTitle tag="h4">Under Construction...</CardTitle>
+                <CardTitle tag="h4">The project</CardTitle>
             </CardHeader>
+            <CardBody>
+                The present software blablabla...
+            </CardBody>
             </Card>
             </Col>
         </Row>
+
+        <Row>
+            <Col>
+            <Card>
+            <CardHeader>
+                <CardTitle tag="h4">Who we are</CardTitle>
+            </CardHeader>
+            <CardBody>
+                <Row>
+                <Col>
+                <Card>
+                    <CardImg top src={require("assets/img/felipe.png")} />
+                    <CardHeader><CardTitle tag="h6">Felipe Colombelli</CardTitle></CardHeader>
+                    <CardBody>Undergraduate student blablabla</CardBody>
+                </Card>
+                </Col>
+                <Col>
+                <Card>
+                    <CardImg top src={require("assets/img/mariana.jpg")} />
+                    <CardHeader><CardTitle tag="h6">Mariana Recamonde Mendoza</CardTitle></CardHeader>
+                    <CardBody>Professor Phd blablabla</CardBody>
+                </Card>
+                </Col>
+                </Row>
+            </CardBody>
+            </Card>
+            </Col>
+        </Row>
+
         </div>
         </>
 
